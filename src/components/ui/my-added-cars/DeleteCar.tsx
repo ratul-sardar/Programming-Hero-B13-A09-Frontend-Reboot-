e61f434909728c80d4c298be058212b3344e2c21
ratul-sardar <ratulsardar4747@gmail.com>
2026-09-22 00:13:20 +0600

    added the delete confirmation modal

--- src/components/ui/my-added-cars/DeleteCar.tsx
+++ src/components/ui/my-added-cars/DeleteCar.tsx
@@ -1,18 +1,54 @@
 "use client";
 
-import { Button } from "@heroui/react";
+import { TrashBin } from "@gravity-ui/icons";
+import { Button, Modal } from "@heroui/react";
+import { useRouter } from "next/router";
 import { deleteCar } from "@/app/api/cars.api";
 
-export default function DeleteCar({
-	id,
-	children,
-}: {
+type propsType = {
 	id: string;
 	children: React.ReactNode;
-}) {
+};
+
+export default function DeleteCar({ id, children }: propsType) {
+	const router = useRouter();
+
+	async function deleteThisCar(id: string) {
+		deleteCar(id);
+		router.reload();
+	}
+
 	return (
-		<Button variant="danger" onClick={() => deleteCar(id)}>
-			{children}
-		</Button>
+		<Modal>
+			<Button variant="secondary">{children}</Button>
+			<Modal.Backdrop>
+				<Modal.Container>
+					<Modal.Dialog className="sm:max-w-90">
+						<Modal.CloseTrigger />
+						<Modal.Header>
+							<Modal.Icon className="bg-default text-foreground">
+								<TrashBin className="size-5" />
+							</Modal.Icon>
+							<Modal.Heading>Delete car!</Modal.Heading>
+						</Modal.Header>
+						<Modal.Body>
+							<p>
+								Are you sure you want to delete this car? This can't be undone!
+							</p>
+						</Modal.Body>
+						<Modal.Footer>
+							<Button
+								className="w-full"
+								slot="close"
+								variant="danger"
+								onClick={() => deleteThisCar(id)}
+							>
+								Continue
+							</Button>
+						</Modal.Footer>
+					</Modal.Dialog>
+				</Modal.Container>
+			</Modal.Backdrop>
+		</Modal>
 	);
 }
